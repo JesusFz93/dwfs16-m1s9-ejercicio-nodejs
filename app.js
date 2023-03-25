88664d3bc92a91270f273a7548d415f6e7c0ed43
Jesus <jesus.fdz93@gmail.com>
2023-03-25 13:44:43 -0500

    ejercicio1

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const calculadora = require("./calculadora")
 
-const a = 15;
+const a = 10;
 const b = 5;
 
 const resultadoResta = calculadora.resta(a, b);
 const resultadoDivision = calculadora.division(a, b);
+const resultadoSuma = calculadora.suma(a, b);
+const resultadoMultiplicacion = calculadora.multiplicacion(a, b);
 
 console.log(`La resta de ${a} y ${b} es: ${resultadoResta}`);
 console.log(`La division de ${a} y ${b} es: ${resultadoDivision}`);
+console.log(`La suma de ${a} y ${b} es: ${resultadoSuma}`);
+console.log(`La multiplicacion de ${a} y ${b} es: ${resultadoMultiplicacion}`);
--- calculadora.js
+++ calculadora.js
@@ -6,6 +6,14 @@ const division = (num1, num2) => {
     return num1 / num2
 }
 
+const suma = (num1, num2) => {
+    return num1 + num2
+}
+
+const multiplicacion = (num1, num2) => {
+    return num1 * num2
+}
+
 module.exports = {
-    resta, division,
+    resta, division, suma, multiplicacion
 }
